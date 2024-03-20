65fa4f260fd3e936e5b9fe920548bec6bb9a1246
VoidBaroness <52724070+GamingInfinite@users.noreply.github.com>
2024-03-20 17:28:54 -0500

    update

--- rule34fitscreen.js
+++ rule34fitscreen.js
@@ -1,11 +1,3 @@
-let container = document.getElementsByClassName("flexi")[0];
+let video = document.getElementById("gelcomVideoContainer");
 
-let store;
-for (let i = 0; i < container.children[0].children.length; i++) {
-  const element = container.children[0].children[i];
-  if (element.id == "image" || element.id == "gelcomVideoContainer") {
-    store = element;
-    break;
-  }
-}
-store.style.height = (screen.height / 6) * 4 + "px";
+video.style.height = "70vh";
